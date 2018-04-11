6fe7f10a13b481f12c6d9ee0894172af9cc874ca
AteaASGitHub <38223160+AteaASGitHub@users.noreply.github.com>
2018-04-11 15:30:30 +0300

    Add files via upload

---
+++ templateAteaReportGroupCountUsers.ps1
@@ -0,0 +1,14 @@
+﻿#Käesolev skript on paigaldatud Atea AS-i poolt lepinguliste raporteerimiskohustuste täitmiseks.
+#Kontaktid
+#E-mail: hooldus@atea.ee
+#Telefon: +372 610 5924
+#
+#This script is put in place and maintained by Atea AS to fulfill obligations taken by contract.
+#Contacts
+#E-mail: service@atea.ee
+#Phone: +372 610 5924
+
+$group = Get-ADGroup -identity "grupinimiAD-s"
+$countUser = (Get-ADGroupMember $group.DistinguishedName).count 
+add-content "c:\scripts\AteaReportGroupCountUsers.txt" $countUser
+
