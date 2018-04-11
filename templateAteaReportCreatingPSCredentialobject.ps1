d276278b84d9d2b6bb1373a6090045e433aea5f7
AteaASGitHub <38223160+AteaASGitHub@users.noreply.github.com>
2018-04-11 11:59:36 +0300

    Add files via upload

---
+++ templateAteaReportCreatingPSCredentialobject.ps1
@@ -0,0 +1,6 @@
+﻿$User = "kasutajakonto"
+$PasswordFile = "Atea_Report_vCenter_VM_vCPU_vRAM_HDD_Password.txt"
+$KeyFile = "Atea_Report_vCenter_VM_vCPU_vRAM_HDD_AES.key"
+$key = Get-Content $KeyFile
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential `
+ -ArgumentList $User, (Get-Content $PasswordFile | ConvertTo-SecureString -Key $key)
