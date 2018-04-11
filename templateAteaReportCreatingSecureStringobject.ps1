3299b83d949cf699ac559ac59a19882fee54f0b2
AteaASGitHub <38223160+AteaASGitHub@users.noreply.github.com>
2018-04-11 11:56:27 +0300

    Add files via upload

---
+++ templateAteaReportCreatingSecureStringobject.ps1
@@ -0,0 +1,5 @@
+﻿$PasswordFile = "Atea_Report_vCenter_VM_vCPU_vRAM_HDD_Password.txt"
+$KeyFile = "Atea_Report_vCenter_VM_vCPU_vRAM_HDD_AES.key"
+$Key = Get-Content $KeyFile
+$Password = "keerulineAb0#parool" | ConvertTo-SecureString -AsPlainText -Force
+$Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
