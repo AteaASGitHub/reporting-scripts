a59d700b3233b3120699b14341ffa2e218d8c1ac
AteaASGitHub <38223160+AteaASGitHub@users.noreply.github.com>
2018-04-11 11:53:27 +0300

    Add files via upload

---
+++ templateAteaReportCreatingAESkeywithrandomdataandexporttofile.ps1
@@ -0,0 +1,4 @@
+﻿$KeyFile = "Atea_Report_vCenter_VM_vCPU_vRAM_HDD_AES.key"
+$Key = New-Object Byte[] 16   # You can use 16, 24, or 32 for AES
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Key | out-file $KeyFile
